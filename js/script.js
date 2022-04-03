4e4afd46c7f95733ac5af8682eadd504e86e6f5c
yusuf0786 <74721122+yusuf0786@users.noreply.github.com>
2022-04-03 19:57:36 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// targetting the elements
+let aqwal = document.querySelector("#aqwal")
+
+/* in the function below, 
+targetting the values of the targetted elements 
+and putting the code text in the value of textarea
+*/
+function myfunction() {
+
+  let aqwal_value = aqwal.value
+
+  let generate_code = `
+  <li><span>*</span><span><q>${aqwal_value}</q></span></li>`
+
+let mycode = document.querySelector("#mycode")
+mycode.value = generate_code
+}
+myfunction()
+
+// running the function while inputing in the input
+aqwal.addEventListener('input', myfunction)
+
+/* copieng the value inside the textarea using the
+copy_btn */
+copy_btn.addEventListener('click', function(e){
+  e.preventDefault()
+  myFunction()
+})
+
+/* below is the function which can, when run copy
+the value of targetted element */
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("mycode");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+}
